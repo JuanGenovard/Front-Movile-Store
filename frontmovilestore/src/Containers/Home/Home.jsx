d56a83dd5fdf9d1a08b0de8bff47fb62e256226b
Juan Genovard <juangenovardcaballero@gmail.com>
2022-12-29 19:29:25 +0100

    cambios en el titulo

--- frontmovilestore/src/Containers/Home/Home.jsx
+++ frontmovilestore/src/Containers/Home/Home.jsx
@@ -10,15 +10,18 @@ function Home() {
     <div className="cont container-fluid">
       <div className="row">
         <div className="col titulocol d-flex justify-content-center align-items-center">
-          <p>MobileStore</p>
+          <div className='mobile'>Mobile</div>
+          <div>Store</div>
         </div>
       </div>
       <div class="row g-0 text-center">
         <div class="col-sm-6 col-md-6 coltext d-flex justify-content-center align-items-center">
           <div className='divtext1 d-flex justify-content-center align-items-center text'>
-              ¡Estás en Movile Store,<br />
+
+              ¡Estás en MovileStore,<br />
               la mejor web dedicada al<br />
               reacondicionado!
+
           </div>
         </div>
         <div class="col-sm-6 col-md-6 coltext d-flex flex-column justify-content-center align-items-center colbotons">

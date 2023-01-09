93d3841ed7920167c9e2687f6f48ee97085acb2e
Juan Genovard <juangenovardcaballero@gmail.com>
2023-01-09 20:06:21 +0100

    ya no hay scroll ni lateral ni vertical, dejandolo bonito

--- frontmovilestore/src/Containers/Home/Home.jsx
+++ frontmovilestore/src/Containers/Home/Home.jsx
@@ -13,23 +13,21 @@ function Home() {
       <Row>
         <Col sm={12} md={6}  className="titulocol d-flex justify-content-center align-items-center">
           <div className='mobile'>Mobile</div>
-          <div>Store</div>
+          <div className='store'>Store</div>
         </Col>
       </Row>
       <Row className="text-center">
-        <Col sm={12} md={6} className="coltext d-flex justify-content-center align-items-center">
           <Col sm={12} md={6} className='divtext1 d-flex justify-content-center align-items-center text'>
             ¡Estás en MovileStore,<br />
             la mejor web dedicada al<br />
             reacondicionado!
-          </Col>
         </Col>
-        <Col sm={12} md={6} className="coltext d-flex flex-column justify-content-center align-items-center colbotons">
+        <Col sm={12} md={6} className="coltext d-flex flex-column justify-content-center align-items-center">
           <Col sm={12} md={6} className='divtext2 d-flex justify-content-center align-items-center'>
             <Link className='wordlogin' to='/login'>LOGIN</Link>
           </Col>
           <div className='divtext2 d-flex justify-content-center align-items-center'>
-            <Link className='wordregister' to='/register'>REGISTER</Link>
+            <Link className='wordlogin' to='/register'>REGISTER</Link>
           </div>
         </Col>
       </Row>
@@ -56,12 +54,12 @@ function Home() {
         </Row>
         <Row>
           <Col sm={12} md={6}  className='d-flex justify-content-center align-items-center'>
-            <img className='imagenes' src={logo} alt="imagen" />
+            <img className='imagenes' src={logo3} alt="imagen" />
           </Col>
           <Col sm={12} md={6}  className='d-flex justify-content-center align-items-center'>
-            <h4> En nuestra MovilStore encontrarás<br />
-              una gran variedad de móviles de<br />
-              alta gama a un precio increíble.</h4>
+            <h4> También contamos con un gran<br />
+             soporte técnico y una garantía<br />
+              de hasta dos años.</h4>
           </Col>
         </Row>
       </Container>

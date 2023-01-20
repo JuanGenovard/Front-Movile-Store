341dad80d43485a0a5802146994fa87b9f7cdb3d
Juan Genovard <juangenovardcaballero@gmail.com>
2023-01-20 16:28:20 +0100

    provando delete

--- src/Containers/Movil/MovilDetails.jsx
+++ src/Containers/Movil/MovilDetails.jsx
@@ -10,10 +10,13 @@ import './MovilDetails.scss'
 
 
 
+
+
 const MovilDetail = () => {
     const { id } = useParams();
     const token = localStorage.getItem('jwt');
     let decoded = jwt_decode(token);
+    const isAdmin = JSON.parse(localStorage.getItem('isAdmin'));
 
     const [movil, setMovil] = useState(null)
     const [cargando, setCargando] = useState(true)
@@ -26,7 +29,17 @@ const MovilDetail = () => {
         const config = {
             headers: { Authorization: `Bearer ${token}` },
         };
-        axios.post('http://localhost:3001/compras/nuevocompras', comprasbody, config)
+        axios.post('https://proyectofinal-production-63cf.up.railway.app/compras/nuevocompras', comprasbody, config)
+            .then(response => {
+            })
+            .catch(error => {
+            });
+    }
+    function deleteClick() {
+        const config = {
+            headers: { Authorization: `Bearer ${token}` },
+        };
+        axios.delete(`https://proyectofinal-production-63cf.up.railway.app/movils/movils/${id}`,config)
             .then(response => {
             })
             .catch(error => {
@@ -35,7 +48,7 @@ const MovilDetail = () => {
 
     useEffect(() => {
         if (movil === null) {
-            axios.get(`http://localhost:3001/movils/${id}`).then(response => {
+            axios.get(`https://proyectofinal-production-63cf.up.railway.app/movils/${id}`).then(response => {
                 setMovil(response.data)
                 setCargando(false)
             })
@@ -46,6 +59,23 @@ const MovilDetail = () => {
     if (cargando) {
         return <div>Cargando</div>
     }
+    if(isAdmin) {
+        return (
+            movil ? <div className="movildiv d-flex justify-content-center align-items-center">
+                <img className="movilbox" src={movil.URL} alt="imagen" /><br />
+                <div className="divinfo d-flex flex-column">
+                <div className="nombreinfo">{movil.nombre}</div><br />
+                <div className="nombreinfo">{movil.color}</div><br />
+                <div className="nombreinfo">{movil.precio}</div><br />
+                </div>
+                <Link className='divcomprar m-5 d-flex justify-content-center align-items-center' to="/venta" onClick={() => deleteClick()}>
+                    <h1> Eliminar </h1>
+                </Link>
+            </div> : <div>No encontrado</div>
+    
+        );
+    }
+    else{
 
     return (
         movil ? <div className="movildiv d-flex justify-content-center align-items-center">
@@ -61,7 +91,8 @@ const MovilDetail = () => {
         </div> : <div>No encontrado</div>
 
     );
-}
+}}
+
 
 
 

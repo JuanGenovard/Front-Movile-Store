22613cdb5a8aa50ecd759ce1fadfeee163ac194d
Juan Genovard <juangenovardcaballero@gmail.com>
2023-01-22 11:54:41 +0100

    listo

--- src/Containers/Compras/Compras.jsx
+++ src/Containers/Compras/Compras.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { useJwt } from "react-jwt";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
-import MovilBox from '../../Components/MovilBox/MovilBox';
-import { Container, Row, Col, } from 'react-bootstrap';
 
 
 const Compras = () => {
@@ -13,7 +11,7 @@ const Compras = () => {
     const isAdmin = JSON.parse(localStorage.getItem("isAdmin"));
     const config = {
         headers: { Authorization: `Bearer ${token}` },
-    };
+      };
     const [compras, setCompras] = useState([]);
 
     useEffect(() => {
@@ -28,26 +26,16 @@ const Compras = () => {
     }, [])
 
     return (
-        <Container>
-            <Row className='d-flex'>
-                {compras.map((compra) => {
-                    return(
-                    
-                        <Col xs={12} sm={5} md={4} lg={3}>
-                            <MovilBox>
-                        {compra.emailUsuario}<br />
-                        {compra.createdAt}<br />
-                        {compra.id_compra}<br />
-                        <img src={compra.movil.URL} /> 
-                        </MovilBox>
-                    
-                        </Col>
-                    
-                    )
-                }
-                )}
-            </Row>
-        </Container>
+        <div>
+            {compras.map((compra) =>
+                <div>
+                    {compra.emailUsuario}<br />
+                    {compra.createdAt}<br />
+                    {compra.id_compra}<br />
+                    <img src={compra.movil.URL}/>
+                </div>
+            )}
+        </div>
     )
 }
 

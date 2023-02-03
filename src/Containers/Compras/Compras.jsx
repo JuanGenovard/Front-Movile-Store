305d30abd60eb335e3d67f22783e90570714551d
Juan Genovard <juangenovardcaballero@gmail.com>
2023-02-03 13:04:50 +0100

    puesto en produccion otra vez

--- src/Containers/Compras/Compras.jsx
+++ src/Containers/Compras/Compras.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Compras = () => {
-    const API_URL = "http://localhost:3001/compras/";
+    const API_URL = "http://proyectofinal-production-63cf.up.railway.app/compras/";
     const navigate = useNavigate()
     const token = localStorage.getItem("jwt");
     const isAdmin = JSON.parse(localStorage.getItem("isAdmin"));

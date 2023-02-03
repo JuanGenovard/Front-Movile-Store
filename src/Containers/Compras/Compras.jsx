16f430ae1d93ec982fbcb935f5f779a0e7b2db79
Juan Genovard <juangenovardcaballero@gmail.com>
2023-02-03 13:17:00 +0100

    faltava una s

--- src/Containers/Compras/Compras.jsx
+++ src/Containers/Compras/Compras.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Compras = () => {
-    const API_URL = "http://proyectofinal-production-63cf.up.railway.app/compras/";
+    const API_URL = "https://proyectofinal-production-63cf.up.railway.app/compras/";
     const navigate = useNavigate()
     const token = localStorage.getItem("jwt");
     const isAdmin = JSON.parse(localStorage.getItem("isAdmin"));

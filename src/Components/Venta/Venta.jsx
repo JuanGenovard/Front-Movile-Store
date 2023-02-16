1b790a7745172a24f9bc7b29dd1fb45f105c1271
Juan Genovard <juangenovardcaballero@gmail.com>
2023-02-16 12:50:10 +0100

    tocando enhorabuena

--- src/Components/Venta/Venta.jsx
+++ src/Components/Venta/Venta.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import logo from '../../Resouces/congratulations.jpg'
+import "./Venta.scss"
 
 function Venta() {
     return (
-        <Container>
-            <Row className="d-flex justify-content-center -aling-items-center">
-                <Col className="d-flex justify-content-center -aling-items-center">
-                    <h1 className="d-flex justify-content-center -aling-items-center">Enhorabuena,<br />
-                     acabas de comprar un magnifico<br />
-                      movil al mejor precio.</h1>
-                      <img className="d-flex justify-content-center -aling-items-center" src={logo} alt="imagen" />
+        <Container className="contenhorabuena">
+            <Row>
+                <Col xs={12} md={24}>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <img className="" src={logo} alt="imagen" />
+                    </div>
+                    <div className="divenhorabuena d-flex justify-content-center align-items-center">
+                        <p className="textenhorabuena"><br />
+                            Acabas de comprar un magnifico<br />
+                            movil al mejor precio.
+                        </p>
+                    </div>
+
+
+
                 </Col>
             </Row>
         </Container>
